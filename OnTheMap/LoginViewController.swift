995d06cdb0a3a0722f3d16b86ca346d9d87f93ed
Jeffersonbe <jefferson.bonnaire@gmail.com>
2016-01-10 18:18:10 +0000

    Add Udacity Login

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Jefferson Bonnaire on 07/01/2016.
+//  Copyright © 2016 Jefferson Bonnaire. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var facebookLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    @IBAction func login(sender: AnyObject) {
+        UdacityClient.sharedInstance().username = emailTextField.text!
+        UdacityClient.sharedInstance().password = passwordTextField.text!
+        UdacityClient.sharedInstance().loginAndCreateSession() { (success, errorString) in
+            if success {
+                self.completeLogin()
+            } else {
+                dispatch_async(dispatch_get_main_queue(), {
+                    let errorAlert = UIAlertController(title: errorString!, message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+                    errorAlert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(errorAlert, animated: true, completion: nil)
+                })
+            }
+        }
+    }
+
+    // finish login if UdacityClient API call was successful
+    func completeLogin() {
+        dispatch_async(dispatch_get_main_queue(), {
+            let rootNavVC = self.storyboard!.instantiateViewControllerWithIdentifier("rootNavVC") as! UINavigationController
+            self.presentViewController(rootNavVC, animated: true, completion: nil)
+        })
+    }
+}
---
+++ OnTheMap/UdacityClient.swift
@@ -0,0 +1,147 @@
+//
+//  UdacityClient.swift
+//  OnTheMap
+//
+//  Created by Jefferson Bonnaire on 10/01/2016.
+//  Copyright © 2016 Jefferson Bonnaire. All rights reserved.
+//
+
+import Foundation
+
+class UdacityClient: NSObject {
+
+    // MARK: - Variables
+
+    var username: String = ""
+    var password: String = ""
+    var uniqueID: String = ""
+    var userFirstName: String = ""
+    var userLastName: String = ""
+
+    var session: NSURLSession
+    var completionHandler : ((success: Bool, errorString: String?) -> Void)? = nil
+
+    override init() {
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        session = NSURLSession(configuration: config)
+        super.init()
+    }
+
+    // MARK: - API Functions
+
+    // Login to Udacity with user-supplied credentials
+    func loginAndCreateSession(completionHandler: (success: Bool, errorString: String?) -> Void) {
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = "{\"udacity\": {\"username\": \"\(username)\", \"password\": \"\(password)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil {
+                completionHandler(success: false, errorString: "The internet connection appears to be offline")
+            } else {
+                let newData = data!.subdataWithRange(NSMakeRange(5, data!.length - 5))
+                let parsedResult: AnyObject!
+                do {
+                    parsedResult = try NSJSONSerialization.JSONObjectWithData(newData, options: .AllowFragments)
+                } catch {
+                    parsedResult = nil
+                    print("Could not parse the data as JSON: '\(data)'")
+                    return
+                }
+
+//                guard let _ = parsedResult["error"] as? String else {
+//                    completionHandler(success: false, errorString: "There are error in your request")
+//                    return
+//                }
+
+                guard let registered = parsedResult["account"] as? NSDictionary else {
+                    completionHandler(success: false, errorString: "No user have been found")
+                    return
+                }
+
+                self.uniqueID = registered["key"] as! String
+                self.getUserData()
+                completionHandler(success: true, errorString: nil)
+            }
+        }
+        task.resume()
+    }
+
+    // logout and destroy session
+    func logoutAndDeleteSession(completionHandler: (success: Bool, errorString: String?) -> Void) {
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        request.HTTPMethod = "DELETE"
+        var xsrfCookie: NSHTTPCookie? = nil
+        let sharedCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        for cookie in sharedCookieStorage.cookies! {
+            if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+        }
+        if let xsrfCookie = xsrfCookie {
+            request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+        }
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil {
+                completionHandler(success: false, errorString: "The internet connection appears to be offline")
+            } else {
+                let newData = data!.subdataWithRange(NSMakeRange(5, data!.length - 5))
+                let parsedResult: AnyObject!
+                do {
+                    parsedResult = try NSJSONSerialization.JSONObjectWithData(newData, options: .AllowFragments)
+                } catch {
+                    parsedResult = nil
+                    print("Could not parse the data as JSON: '\(data)'")
+                    return
+                }
+                guard parsedResult["session"] != nil else {
+                    completionHandler(success: false, errorString: "Could not logout")
+                    return
+                }
+                completionHandler(success: true, errorString: nil)
+            }
+        }
+        task.resume()
+    }
+
+    // get user first name and last name
+    func getUserData() {
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/users/\(uniqueID)")!)
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil {
+                return
+            } else {
+                let newData = data!.subdataWithRange(NSMakeRange(5, data!.length - 5))
+                let parsedResult: AnyObject!
+                do {
+                    parsedResult = try NSJSONSerialization.JSONObjectWithData(newData, options: .AllowFragments)
+                } catch {
+                    parsedResult = nil
+                    print("Could not parse the data as JSON: '\(data)'")
+                    return
+                }
+
+                guard let user = parsedResult["user"] as? NSDictionary else {
+                    print("Couldn't find the user")
+                    return
+                }
+
+                self.userFirstName = user["first_name"] as! String
+                self.userLastName = user["last_name"] as! String
+            }
+        }
+        task.resume()
+    }
+
+    // MARK: - Shared Instance
+
+    // make this class a singleton to share across classes
+    class func sharedInstance() -> UdacityClient {
+
+        struct Singleton {
+            static var sharedInstance = UdacityClient()
+        }
+
+        return Singleton.sharedInstance
+    }
+
+}
