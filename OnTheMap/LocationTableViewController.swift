17bc469cd4103f4259910a35125b2c998f8a3dce
Jeffersonbe <jefferson.bonnaire@gmail.com>
2016-01-11 11:43:58 +0000

    Add LocationTableView

---
+++ OnTheMap/LocationTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  LocationTableViewController.swift
+//  OnTheMap
+//
+//  Created by Jefferson Bonnaire on 10/01/2016.
+//  Copyright © 2016 Jefferson Bonnaire. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var table: UITableView!
+
+    var locations: [OTMLocation] = [OTMLocation]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
+        ParseClient.sharedInstance().getLocations { success, locations, error in
+            if let location = locations {
+                self.locations = location
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.table.reloadData()
+                }
+            } else {
+                print(error)
+            }
+        }
+        self.parentViewController!.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Reply, target: self, action: "logoutButtonTouchUp")
+    }
+    
+    // MARK: UITableViewController
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        /* Get cell type */
+        let cellReuseIdentifier = "locationCell"
+        let location = locations[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier) as UITableViewCell!
+
+        /* Set cell defaults */
+        cell.textLabel?.text = "\(location.firstName) \(location.lastName)"
+        cell.detailTextLabel?.text = location.mediaURL
+        return cell
+
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locations.count
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+
+        /* Push the movie detail view */
+//        let controller = self.storyboard!.instantiateViewControllerWithIdentifier("MovieDetailViewController") as! MovieDetailViewController
+//        controller.movie = movies[indexPath.row]
+//        self.navigationController!.pushViewController(controller, animated: true)
+    }
+}
